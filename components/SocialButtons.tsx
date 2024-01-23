c0363133c7316237a6f8f913231f947ab1a59869
Mouad Bounfil <mouad.bounfil.it@gmail.com>
2024-01-23 22:39:51 +0100

    event

--- components/SocialButtons.tsx
+++ components/SocialButtons.tsx
@@ -1,7 +1,7 @@
-import { FaWhatsapp, FaGithub, FaFacebookF , FaYoutube } from "react-icons/fa";
+import { FaWhatsapp, FaGithub, FaFacebookF , FaYoutube , FaLinkedin } from "react-icons/fa";
 import { RiTwitterXFill } from "react-icons/ri";
 
-import { FaLinkedin, FaInstagram, FaEnvelope } from "react-icons/fa";
+import { FaInstagram, FaEnvelope } from "react-icons/fa";
 import { Button } from "./ui/button";
 
 import React, { useState } from "react";
@@ -15,17 +15,10 @@ const SocialButtons = ({ data, phone ,sendMessageWa, sendEmail }) => {
   const getSocialIcon = (key) => {
     switch (key) {
       case "whatsapp":
-        return <FaWhatsapp className="h-5 w-5" />;
+        return <FaWhatsapp className="h-4 w-4" />;
       case "linkedIn":
         return (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-4 w-4"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
-          </svg>
+          <FaLinkedin className="h-4 w-4"/>
         );
       case "instagram":
         return <FaInstagram className="h-4 w-4" />;
@@ -122,7 +115,7 @@ const SocialButtons = ({ data, phone ,sendMessageWa, sendEmail }) => {
               className="animate-fade-right animate-delay-300 flex justify-between items-center gap-2 p-2 bg-white text-[#1d1d1d] border-[1px] border-solid hover:bg-gray-200 border-[#ececec] rounded-[10px]"
             >
               {icon}
-              <span className="text-[16px]">{text}</span>
+              <span className="text-[17px]">{text}</span>
             </Button>
           </a>
         )
