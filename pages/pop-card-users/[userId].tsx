2551f7255895d011e5a6cd96a229d53ad04dc2b0
Mouad Bounfil <mouad.bounfil.it@gmail.com>
2024-01-25 14:35:13 +0100

    badge

--- pages/pop-card-users/[userId].tsx
+++ pages/pop-card-users/[userId].tsx
@@ -359,7 +359,7 @@ const ProfilePage: React.FC = () => {
                               <GrTechnology className="w-4 h-4 mr-2 " />
                               <span className="relative">
                                 Technologies :
-                                <span className="absolute bottom-0 left-0 w-full border-b border-black"></span>
+                                <span className="absolute bottom-[0.5px] left-0 w-full border-b border-black"></span>
                               </span>
                             </div>
                             <div className="flex  items-center flex-wrap ">
@@ -387,7 +387,7 @@ const ProfilePage: React.FC = () => {
                               
                               <span className="relative">
                               Industries :
-                                <span className="absolute bottom-0 left-0 w-full border-b border-black"></span>
+                                <span className="absolute bottom-[0.5px] left-0 w-full border-b border-black"></span>
                               </span>
                             </div>{" "}
                             
@@ -415,7 +415,7 @@ const ProfilePage: React.FC = () => {
                               <MdOutlineBusinessCenter className="w-5 h-5 mr-2 " />{" "}
                               <span className="relative">
                               Business Models :
-                                <span className="absolute bottom-0 left-0 w-full border-b border-black"></span>
+                                <span className="absolute bottom-[0.5px] left-0 w-full border-b border-black"></span>
                               </span>
                             </div>{" "}
                             <div className="flex  items-center flex-wrap ">
